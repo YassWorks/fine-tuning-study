56c3a88bdc36d3ef110e49ec02609de3b395c6cc
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2025-06-29 21:48:11 +0100

    SFTTrainer class and a few fixes

--- customization/pipeline.py
+++ customization/pipeline.py
@@ -5,24 +5,13 @@ from datasets import Dataset as HFDataset, load_dataset
 from typing import Any
 
 
-MODEL_NAME = ""
-OUTPUT_DIR = "./output"
-
-
-USE_DAFT = True
-USE_SFT = True
-USE_LoRA = True
-
-
-text_gen = TextGenerator(MODEL_NAME)
-tokenizer = text_gen.tokenizer
-model = text_gen.model
+OUTPUT_DIR = "./cache/output"
 
 
 class DAFTTrainer:
     """
     Fine-tunes a given model using the Domain-Adaptive Fine-Tuning (DAFT) approach on a given dataset.
-    Outputs a new fine-tuned model saved to a specified output directory.
+    Outputs a new fine-tuned model that can be saved to a specified output directory.
     """
 
     def __init__(
@@ -69,7 +58,8 @@ class DAFTTrainer:
     def fine_tune(self):
         """
         Fine-tunes the model using the provided dataset. (Domain-Adaptive Fine-Tuning **DAFT** approach)
-        It saves the trained model and tokenizer to the specified output directory.
+        #### Note:
+        This method will change the model's parameters **inplace**.
         """
 
         model = self.text_gen.model
@@ -77,13 +67,11 @@ class DAFTTrainer:
         _dataset = self.dataset.load()
 
         tokenized_train = _dataset.map(
-            DAFTTrainer.preprocesser, remove_columns=_dataset.column_names
+            self.preprocesser, remove_columns=_dataset.column_names
         )
 
-        # Data collator (convenient for batching)
         data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
-        # Trainer args
         training_args = TrainingArguments(
             output_dir=OUTPUT_DIR,
             per_device_train_batch_size=4,
@@ -105,18 +93,14 @@ class DAFTTrainer:
         )
 
         trainer.train()
-        
-        trainer.save_model(OUTPUT_DIR)
-        tokenizer.save_pretrained(OUTPUT_DIR)
+    
 
-
-    @staticmethod
-    def preprocesser(example):
+    def preprocesser(self, example):
         """
         Preprocess function for the dataset.
         """
         full_text = " ".join([str(value) for value in example.values()])
-        encoded = tokenizer(
+        encoded = self.text_gen.tokenizer(
             full_text,
             truncation=True,
             padding="max_length",
@@ -125,3 +109,155 @@ class DAFTTrainer:
         )
         encoded["labels"] = encoded["input_ids"].clone()
         return {k: v.squeeze() for k, v in encoded.items()}
+    
+    
+    def save_model(self, output_dir: str = OUTPUT_DIR):
+        """
+        Saves the model and tokenizer to the specified output directory.
+        Args:
+            output_dir (str): The directory where the model and tokenizer will be saved.
+        """
+        self.text_gen.model.save_pretrained(output_dir)
+        self.text_gen.tokenizer.save_pretrained(output_dir)
+        
+
+class SFTTrainer:
+    """
+    Fine-tunes a given model using the Domain-Adaptive Fine-Tuning (SFT) approach on a given dataset.
+    Outputs a new fine-tuned model that can be saved to a specified output directory.
+    """
+
+    def __init__(
+        self,
+        text_gen: TextGenerator = None,
+        model_name: str = None,
+        train_dataset: DataSet | HFDataset | Any = None,
+        train_dataset_name: str = None,
+        train_dataset_split_name: str = "train",
+        test_dataset: DataSet | HFDataset | Any = None,
+        test_dataset_name: str = None,
+        test_dataset_split_name: str = "test",
+    ):
+        """
+        Initializes the DAFTTrainer with either a TextGenerator instance or a model name, 
+        and either a DataSet instance or a dataset name.
+        Args:
+            text_gen (TextGenerator, optional): An instance of TextGenerator. Defaults to None.
+            model_name (str, optional): The name of the model to use. Defaults to None.
+            train_dataset (DataSet, optional): An instance of DataSet for training. Defaults to None.
+            train_dataset_name (str, optional): The name of the training dataset to use. Defaults to None.
+            train_dataset_split_name (str, optional): The split of the training dataset to use. Defaults to "train".
+            test_dataset (DataSet, optional): An instance of DataSet for testing. Defaults to None.
+            test_dataset_name (str, optional): The name of the testing dataset to use. Defaults to None.
+            test_dataset_split_name (str, optional): The split of the testing dataset to use. Defaults to "test".
+        """
+        if model_name:
+            self.text_gen = TextGenerator(model_name)
+        elif text_gen:
+            if not isinstance(text_gen, TextGenerator):
+                raise ValueError("text_gen must be an instance of TextGenerator")
+            self.text_gen = text_gen
+        else:
+            raise ValueError("Either model_name or text_gen must be provided")
+        
+        if train_dataset_name:
+            try:
+                self.train_dataset = DataSet(train_dataset_name, train_dataset_split_name)
+            except Exception:
+                raise ValueError(f"Please provide a valid dataset split.")
+        elif train_dataset:
+            if isinstance(train_dataset, DataSet):
+                self.train_dataset = train_dataset
+            elif isinstance(train_dataset, HFDataset):
+                self.train_dataset = load_dataset(train_dataset, split=train_dataset_split_name)
+        else:
+            raise ValueError("Either dataset_name or dataset must be provided")
+        
+        if test_dataset_name:
+            try:
+                self.test_dataset = DataSet(test_dataset_name, test_dataset_split_name)
+            except Exception:
+                raise ValueError(f"Please provide a valid dataset split.")
+        elif test_dataset:
+            if isinstance(test_dataset, DataSet):
+                self.test_dataset = test_dataset
+            elif isinstance(test_dataset, HFDataset):
+                self.test_dataset = load_dataset(test_dataset, split=test_dataset_split_name)
+        else:
+            raise ValueError("Either dataset_name or dataset must be provided")
+
+
+    def fine_tune(self):
+        """
+        Fine-tunes the model using the provided dataset. (Supervised Fine-Tuning **SFT** approach)
+        #### Note:
+        This method will change the model's parameters **inplace**.
+        """
+        model = self.text_gen.model
+        tokenizer = self.text_gen.tokenizer
+        _train_dataset = self.train_dataset.load()
+        _test_dataset = self.test_dataset.load()
+
+        tokenized_train = _train_dataset.map(
+            self.preprocesser, remove_columns=_train_dataset.column_names
+        )
+        tokenized_test = _train_dataset.map(
+            self.preprocesser, remove_columns=_test_dataset.column_names
+        )
+
+        data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+        training_args = TrainingArguments(
+            output_dir=OUTPUT_DIR,
+            per_device_train_batch_size=4,
+            per_device_eval_batch_size=4,
+            eval_strategy="steps",
+            eval_steps=500,
+            logging_steps=100,
+            save_strategy="epoch",
+            save_total_limit=2,
+            num_train_epochs=3,
+            learning_rate=5e-5,
+            weight_decay=0.01,
+            warmup_steps=100,
+            fp16=torch.cuda.is_available(),
+            logging_dir=f"{OUTPUT_DIR}/logs",
+            report_to="none",
+            push_to_hub=False
+        )
+
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=tokenized_train,
+            eval_dataset=tokenized_test,
+            data_collator=data_collator
+        )
+
+        trainer.train()
+        
+
+    def preprocesser(self, example):
+        """
+        Preprocess function for the dataset.
+        """
+        full_text = " ".join([str(value) for value in example.values()])
+        encoded = self.text_gen.tokenizer(
+            full_text,
+            truncation=True,
+            padding="max_length",
+            max_length=512,
+            return_tensors="pt"
+        )
+        encoded["labels"] = encoded["input_ids"].clone()
+        return {k: v.squeeze() for k, v in encoded.items()}
+    
+    
+    def save_model(self, output_dir: str = OUTPUT_DIR):
+        """
+        Saves the model and tokenizer to the specified output directory.
+        Args:
+            output_dir (str): The directory where the model and tokenizer will be saved.
+        """
+        self.text_gen.model.save_pretrained(output_dir)
+        self.text_gen.tokenizer.save_pretrained(output_dir)
